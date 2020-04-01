f0f06003c6177f598d56ed08a92ac77bb8c98418
Ruslan <rus.safiullin.01@mail.ru>
2020-04-01 11:26:03 +0300

    Edited toString, add Test

--- src/ru/kpfu/itis/j903/Test.java
+++ src/ru/kpfu/itis/j903/Test.java
@@ -3,7 +3,7 @@ package ru.kpfu.itis.j903;
 public class Test {
 
     public static void main(String[] args) {
-        Polinom p  = new Polinom("/home/almaz/Загрузки/test.txt");
+        Polinom p  = new Polinom("Test/Output.txt");
         System.out.println(p);
 //        p.derivate();
 //        p.deleteOdd();
